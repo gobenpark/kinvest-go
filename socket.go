0ccc01b49b133127fa57e58765c201fe5fd6aca2
Ben <ben@swit.io>
2023-03-24 17:30:57 +0900

    🎨 update realtime socket

--- socket.go
+++ socket.go
@@ -31,66 +31,100 @@ type SocketBody struct {
 type ResponseBody struct {
 	Code string
 	// 채결시간 hh:mm:ss
-	ContractHour time.Time
-	Price        float64
+	ContractHour string
+	Price        string
 	// 전일 대비 부호
-	CompareSign Sign
+	CompareSign string
 	// 전일대비
-	CompareDay int
+	CompareDay string
 	// 전일 대비율
-	CompareRate float32
+	CompareRate string
 	//가중 평균 주식 가격
-	WeightAveragePrice float32
+	WeightAveragePrice string
 	// 시가
-	Open int
+	Open string
 	// 고가
-	High int
+	High string
 	// 저가
-	Low int
+	Low string
 	// 매도호가
-	AskPrice int
+	AskPrice string
 	// 매수호가
-	BisPrice int
+	BisPrice string
 	// 체결 거래량
-	ContractVolume float32
+	ContractVolume string
 	// 누적 거래량
-	AccumulateVolume float32
+	AccumulateVolume string
 	// 누적 거래대금
-	AccumulateTransactionMoney float32
+	AccumulateTransactionMoney string
 	// 매도 체결 건수
-	AskCount float32
+	AskCount string
 	// 매수 체결 건수
-	BidCount float32
+	BidCount string
 	// 순매수 체결 건수
-	PureBidCount float32
+	PureBidCount string
 	// 체결강도
-	VolumePower float32
+	VolumePower string
 	//총 매도 수량
-	TotalAskCounts float32
+	TotalAskCounts string
 	// 총 매수 수량
-	TotalBidCounts float32
+	TotalBidCounts string
 	// 체결 구분
 	ContractDivide string
 	// 매수 비율
-	BidRate float32
+	BidRate string
 	// 전일 거래량 대비 등락율
-	PredayVolumeCompareRate float32
+	PredayVolumeCompareRate string
 	// 시가 시간
 	OpenningTime string
 	// 시가 대비 구분
-	OpenComapreSign Sign
+	OpenComapreSign string
 	// 시가대비
 	OpenComapre string
 	// 최고가 시간
 	HighTime string
 	// 고가대비 구분
-	HighVolumePerSign     Sign
-	HighCompare           float32
-	LowTime               float32
-	LowVolumePerSign      Sign
-	LowComapre            float32
-	BusinessDate          time.Time
-	NewMarketOpCode       string
-	TransactionSuspension bool
-	RemainAsk
+	HighVolumePerSign string
+	// 고가대비
+	HighCompare string
+	// 최저가 시간
+	LowTime string
+	// 저가대비구분
+	LowVolumePerSign string
+	// 저가대비
+	LowComapre string
+	// 영업 일자
+	BusinessDate string
+	// 신 장운영 구분 코드
+	NewMarketOpCode string
+	// 거래정지 여부
+	TransactionSuspension string
+	// 매도호가 잔량
+	RemainAsk string
+	// 매수호가 잔량
+	RemainBid string
+	// 총 매도호가 잔량
+	TotalRemainAsk string
+	// 총 매수호가 잔량
+	TotalRemainBid   string
+	VolumeRotateRate string
+	// 전일 동시간 누적 거래량
+	PreDayTotalVolume string
+	// 전일 동시간 누적 거래량 비율
+	PreDayTotalVolumeRate string
+	// 시간 구분 코드
+	HourClockCode string
+	// 임의 종료 구분코드
+	MarketTermCode string
+	// 정적 VI 발동기준가
+	VIStandardPrice string
+}
+
+func (r *ResponseBody) GetCode() string {
+	return r.Code
+}
+
+func (r *ResponseBody) GetContractHour() time.Time {
+	t, _ := time.Parse("150405", r.ContractHour)
+	return t
 }
