6516817be8ee7c0dcafad945d9586e324153e10b
Ben <ben@swit.io>
2023-03-30 15:26:01 +0900

    ✅  add account test

---
+++ account_test.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func MockClient(t *testing.T) *Kinvest {
+	t.Helper()
+	cli := NewKinvest(&Config{
+		AppKey:    AppKey,
+		SecretKey: SecretKey,
+		Imitation: false,
+		Customer:  Person,
+		Token:     Token,
+		Account:   "63702268",
+	})
+	return cli
+}
+
+func TestAccount_Approval(t *testing.T) {
+	cli := MockClient(t)
+	key, err := cli.ApprovalKey(context.TODO())
+	assert.NoError(t, err)
+	assert.NotEqual(t, key, "")
+	t.Log(key)
+}
+
+func TestAccount_Approval_AccessToken(t *testing.T) {
+	cli := MockClient(t)
+	token, err := cli.AccessToken(context.TODO())
+	assert.NoError(t, err)
+	assert.NotEqual(t, token, "")
+	t.Log(token)
+}
+
+func TestAccount_Approval_RevokeToken(t *testing.T) {
+	cli := MockClient(t)
+	err := cli.RevokeToken(context.TODO())
+	assert.NoError(t, err)
+}
+
+func TestAccount_Approval_PossibleOrder(t *testing.T) {
+	cli := MockClient(t)
+	err := cli.PossibleOrder(context.TODO(), "68", "", "5000", Limit, false, false)
+	assert.NoError(t, err)
+}
+
+func TestAccount_AccountBalance(t *testing.T) {
+	cli := MockClient(t)
+	err := cli.AccountBalance(context.TODO())
+	assert.NoError(t, err)
+}
